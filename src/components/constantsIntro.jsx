9b9ae56ddb564e23bde7c6011ece956c9bc62674
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-04-28 23:08:55 +0530

    sports updated

--- src/components/constantsIntro.jsx
+++ src/components/constantsIntro.jsx
@@ -34,6 +34,16 @@ import saraswathi from "../assets/culturals/saraswathi.jpg"
 import republic from "../assets/culturals/republic.jpg"
 import womens_day from "../assets/culturals/womens_day.jpeg"
 
+
+// sports
+import Silambam from "../assets/sports/Silambam.jpg"
+import cycleRally from "../assets/sports/cycleRally.jpg"
+import boxingmen from "../assets/sports/boxingmen.jpg"
+import ballbad from "../assets/sports/ballbad.jpeg"
+import basketball from "../assets/sports/basketball.jpeg"
+import Volleyballmen from "../assets/sports/Volleyballmen.jpg"
+
+
 export const placements = [
   { id: 1, src: placement1, alt: "Placement 1" },
   { id: 2, src: placement2, alt: "Placement 2" },
@@ -54,224 +64,92 @@ export const placements = [
 ];
 
 
-export const memorialEvents=[
-    {
-        name:"NAAC peer Team Visit",
-        img:naac_team
-    },
-    {
-        name:"NSCET Hackathon - 36 Hours Tech Marathon",
-        img:nscet_hack
-    },
-    {
-        name:"Skill India Award - 2nd Place",
-        img:tn_skills
-    },
-    {
-        name:"Kalam Young Achiever Awards - 24",
-        img:kalam_young
-    },
-    {
-        name:"WG Tech Solution Pvt.ltd Inauguration ceremony",
-        img:wg_tech
-    },
-    {
-        name:"State Level Science Expo",
-        img:science_expo
-    },  
+
+export const memorialEvents = [
+  {
+    name: "NAAC peer Team Visit",
+    img: naac_team
+  },
+  {
+    name: "NSCET Hackathon - 36 Hours Tech Marathon",
+    img: nscet_hack
+  },
+  {
+    name: "Skill India Award - 2nd Place",
+    img: tn_skills
+  },
+  {
+    name: "Kalam Young Achiever Awards - 24",
+    img: kalam_young
+  },
+  {
+    name: "WG Tech Solution Pvt.ltd Inauguration ceremony",
+    img: wg_tech
+  },
+  {
+    name: "State Level Science Expo",
+    img: science_expo
+  },
 ]
 
-export const culturals=[
-    {
-        name:"Annual Day 25",
-        img:annual_day
-    },
-    {
-        name:"78th Independence Day",
-        img:Independence,
-    },
-    {
-        name:"Thiru Onam Celebration",
-        img:onam
-    },
-    {
-        name:"Fresher’s Day",
-        img:freshers
-    },
-    {
-        name:"11th Graduation Day",
-        img:Graduation
-    },
-    {
-        name:"WAVES- 2024",
-        img:waves
-    },{
-        name:"Navaratri, Saraswathi & Ayudh Pooja Celebration",
-        img:saraswathi
-    },{
-        name:"76th Republic Day Celebration",
-        img:republic
-    },
-    {
-        name:"Womens Day Celebration",
-        img:womens_day
-    },
+export const culturals = [
+  {
+    name: "Annual Day 25",
+    img: annual_day
+  },
+  {
+    name: "78th Independence Day",
+    img: Independence,
+  },
+  {
+    name: "Thiru Onam Celebration",
+    img: onam
+  },
+  {
+    name: "Fresher’s Day",
+    img: freshers
+  },
+  {
+    name: "11th Graduation Day",
+    img: Graduation
+  },
+  {
+    name: "WAVES- 2024",
+    img: waves
+  }, {
+    name: "Navaratri, Saraswathi & Ayudh Pooja Celebration",
+    img: saraswathi
+  }, {
+    name: "76th Republic Day Celebration",
+    img: republic
+  },
+  {
+    name: "Womens Day Celebration",
+    img: womens_day
+  },
 ]
-export const Sports=[
-    {
-        name: "OSCAR World Record – Silambam Multiweapons",
-      img: "silambam_record.jpg"
-      },
-      {
-        name: "Drug Awareness Cycle Rally 2025",
-      img: "cycle_rally.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Handball Women",
-      img: "handball_women.jpg"
-      },
-      {
-        name: "CFI Cycling National Championships",
-      img: "cycling_nationals.jpg"
-      },
-      {
-        name: "All India Inter University Boxing",
-      img: "boxing_interuniversity.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Power Lifting",
-      img: "powerlifting.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Cross Country Men",
-      img: "crosscountry_men.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Cross Country Women",
-      img: "crosscountry_women.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Weight Lifting Men",
-      img: "weightlifting_men.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Ball Badminton Women",
-      img: "ballbadminton_women.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Athletics",
-      img: "athletics_zone17.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Boxing Men",
-      img: "boxing_men.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Athletics",
-      img: "athletics_interzone.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Volleyball Women",
-      img: "volleyball_women.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Volleyball Men",
-      img: "volleyball_men.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Taekwondo Men",
-      img: "taekwondo_men.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Chess Men",
-      img: "chess_men.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Chess Women",
-      img: "chess_women.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Table Tennis Women",
-      img: "tabletennis_women.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Kabbadi Men",
-      img: "kabbadi_men.jpg"
-      },
-      {
-        name: "District Cycling Championship",
-      img: "district_cycling.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Basketball Women",
-      img: "basketball_women.jpg"
-      },
-      {
-        name: "CM Trophy Athletics (Men)",
-      img: "cmtrophy_athletics_men.jpg"
-      },
-      {
-        name: "CM Trophy Silambam (Men)",
-      img: "cmtrophy_silambam_men.jpg"
-      },
-      {
-        name: "CM Trophy Badminton Doubles (Men)",
-      img: "cmtrophy_badminton_doubles.jpg"
-      },
-      {
-        name: "CM Trophy Swimming (Men)",
-      img: "cmtrophy_swimming_men.jpg"
-      },
-      {
-        name: "CM Trophy Handball (Men)",
-      img: "cmtrophy_handball_men.jpg"
-      },
-      {
-        name: "CM Trophy Chess (Men)",
-      img: "cmtrophy_chess_men.jpg"
-      },
-      {
-        name: "CM Trophy Basketball (Women)",
-      img: "cmtrophy_basketball_women.jpg"
-      },
-      {
-        name: "State Cycling Championship",
-      img: "state_cycling.jpg"
-      },
-      {
-        name: "Anna University Zone-17 Basketball Men",
-      img: "basketball_men.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Handball Women – Selection Committee Member",
-      img: "handball_women_selection.jpg"
-      },
-      {
-        name: "Anna University Inter Zone Handball Women – Team Manager",
-      img: "handball_women_manager.jpg"
-      },
-      {
-        name: "Zone-17 Team Manager",
-      img: "zone17_teammanager.jpg"
-      },
-      {
-        name: "Zone-17 Volleyball Women – Selection Committee Member",
-      img: "volleyball_women_selection.jpg"
-      },
-      {
-        name: "Zone-17 Chess Men & Women – Selection Committee Member",
-      img: "chess_selection.jpg"
-      },
-      {
-        name: "Basketball Women – Selection Committee Member",
-      img: "basketball_women_selection.jpg"
-      },
-      {
-        name: "Badminton Women – Selection Committee Member",
-      img: "badminton_women_selection.jpg"
-      },
-      {
-        name: "Basketball Men – Selection Committee Member",
-      img: "basketball_men_selection.jpg"
-      }    
+export const Sports = [
+ 
+  {
+    name: "Drug Awareness Cycle Rally 2025",
+    img: cycleRally
+  },
+  {
+    name: "Anna University Inter Zone Handball Men & Women",
+    img: boxingmen
+  },
+  {
+    name: "Anna University Zone-17 Ball Badminton Women",
+    img: ballbad
+  },
+
+  {
+    name: "Anna University Zone-17 basketball Women",
+    img: basketball
+  },
+  {
+    name: "Anna University Zone-17 Volleyball Men",
+    img: Volleyballmen
+  },
+
 ]
