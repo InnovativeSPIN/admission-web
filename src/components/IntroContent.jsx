6955f2fed8712a7bf83714032c88fec315936270
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-04-28 20:45:26 +0530

    culturals updated

---
+++ src/components/IntroContent.jsx
@@ -0,0 +1,181 @@
+import {React,useState} from 'react'
+import collegeVideo from '../assets/main/collegedrone.mp4';
+import { IoArrowBack } from "react-icons/io5";
+//swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import {memorialEvents,placements,culturals} from "./constantsIntro"
+
+const IntroContent = ({ onBack }) => {
+    const [activeSection, setActiveSection] = useState('events');
+  return (
+    <>
+      <div className="bg_video bg_vide_2">
+        <video autoPlay muted loop playsInline>
+          <source src={collegeVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <section className="main_intro">
+      
+        <div className='main_intro_content'>
+            <div className="main_intro_top">
+            {
+              activeSection === "events" && (
+                <>
+                <div className='intro_content_head'>
+          <h2>
+          memorial events
+          </h2>
+        </div>
+           <Swiper
+               modules={[Navigation, Pagination, Autoplay]}
+               spaceBetween={20}
+               slidesPerView={1}
+               autoplay={{ delay: 3000 }}
+               loop={true}
+               breakpoints={{
+                 640: { slidesPerView: 1 },
+                 768: { slidesPerView: 2 },
+                 1024: { slidesPerView: 3 },
+               }}
+             >
+               {memorialEvents.map((con, id) => (
+                 <SwiperSlide key={id}>
+                   <div className="college_achievement_card ">
+                     {con.img && (
+                       <img src={con.img} alt={`college_Achievement ${id + 1}`} className="college_achievement_img slider_img" />
+                     )}
+                     <p className="college_achievement_text">{con.name}</p>
+                   </div>
+                 </SwiperSlide>
+               ))}
+             </Swiper>
+             </>
+              )
+
+              
+            }
+            {
+              activeSection === "culturals" && (
+                <>
+                <div className='intro_content_head'>
+          <h2>
+          culturals
+          </h2>
+        </div>
+           <Swiper
+               modules={[Navigation, Pagination, Autoplay]}
+               spaceBetween={20}
+               slidesPerView={1}
+               autoplay={{ delay: 3000 }}
+               loop={true}
+               breakpoints={{
+                 640: { slidesPerView: 1 },
+                 768: { slidesPerView: 2 },
+                 1024: { slidesPerView: 3 },
+               }}
+             >
+               {culturals.map((con, id) => (
+                 <SwiperSlide key={id}>
+                   <div className="college_achievement_card ">
+                     {con.img && (
+                       <img src={con.img} alt={`college_Achievement ${id + 1}`} className="college_achievement_img slider_img" />
+                     )}
+                     <p className="college_achievement_text">{con.name}</p>
+                   </div>
+                 </SwiperSlide>
+               ))}
+             </Swiper>
+             </>
+              )
+
+              
+            }
+            {
+              activeSection==="placements"&&(
+                <>
+                <div className='intro_content_head'>
+          <h2>
+          Placed Students 2024-2025
+          </h2>
+        </div>
+           <Swiper
+               modules={[Navigation, Pagination, Autoplay]}
+               spaceBetween={20}
+               slidesPerView={1}
+               autoplay={{ delay: 3000 }}
+               loop={true}
+               breakpoints={{
+                 640: { slidesPerView: 1 },
+                 768: { slidesPerView: 1 },
+                 1024: { slidesPerView: 1 },
+               }}
+             >
+               {placements.map((con, id) => (
+                 <SwiperSlide key={id}>
+                   <div className="placement_card ">
+                     {con.src && (
+                       <img src={con.src} alt={`placement ${id + 1}`} className="placement_img " />
+                     )}
+                   </div>
+                 </SwiperSlide>
+               ))}
+             </Swiper>
+                </>
+              )
+            }
+
+                     
+
+
+
+            </div>
+            {/* bottom */}
+            <div className='main_intro_bottom'>
+            <div className="college_main_btns">
+              <div className="college_main_btn" onClick={() => setActiveSection('placements')}>
+                <h2>
+                Placements
+                </h2>
+              </div>
+              <div className="college_main_btn" onClick={() => setActiveSection('sports')} >
+                <h2>
+                Sports
+                </h2>
+              </div>
+              <div className="college_main_btn" onClick={() => setActiveSection('culturals')}>
+                <h2>
+                culturals
+                </h2>
+              </div>
+              <div className="college_main_btn" onClick={() => setActiveSection('events')}>
+                <h2>
+                Events
+                </h2>
+              </div>
+              <div className="college_main_btn" onClick={() => setActiveSection('dept')} >
+                <h2>
+                S & H Department
+                </h2>
+              </div>
+              
+            </div>
+            </div>
+
+           
+
+
+        </div>
+      </section>
+      <div className="back_icon" onClick={onBack}>
+        <IoArrowBack />
+      </div>
+    </>
+  )
+}
+
+export default IntroContent
---
+++ src/components/constantsIntro.jsx
@@ -0,0 +1,277 @@
+// memorial events
+import naac_team from "../assets/college/naac_peer.JPG"
+import nscet_hack from "../assets/college/nscet_hack.JPG"
+import tn_skills from "../assets/college/tn_skills.jpeg"
+import kalam_young from "../assets/college/kalam_young.jpg"
+import wg_tech from "../assets/college/wg_tech.JPG"
+import science_expo from "../assets/college/science_expo.JPG"
+// placements
+import placement1 from "../assets/placement/placement (1).png";
+import placement2 from "../assets/placement/placement (2).png";
+import placement3 from "../assets/placement/placement (3).png";
+import placement4 from "../assets/placement/placement (4).png";
+import placement5 from "../assets/placement/placement (5).png";
+import placement6 from "../assets/placement/placement (6).png";
+import placement7 from "../assets/placement/placement (7).png";
+import placement8 from "../assets/placement/placement (8).png";
+import placement9 from "../assets/placement/placement (9).png";
+import placement10 from "../assets/placement/placement (10).png";
+import placement11 from "../assets/placement/placement (11).png";
+import placement12 from "../assets/placement/placement (12).png";
+import placement13 from "../assets/placement/placement (13).png";
+import placement14 from "../assets/placement/placement (14).png";
+import placement15 from "../assets/placement/placement (15).png";
+import placement16 from "../assets/placement/placement (16).png";
+
+// culturals
+import annual_day from "../assets/culturals/annualday.JPG"
+import Independence from "../assets/culturals/independance.jpg"
+import onam from "../assets/culturals/onam.JPG"
+import freshers from "../assets/culturals/freshers.JPG"
+import Graduation from "../assets/culturals/Graduation.JPG"
+import waves from "../assets/culturals/waves.JPG"
+import saraswathi from "../assets/culturals/saraswathi.jpg"
+import republic from "../assets/culturals/republic.jpg"
+import womens_day from "../assets/culturals/womens_day.jpeg"
+
+export const placements = [
+  { id: 1, src: placement1, alt: "Placement 1" },
+  { id: 2, src: placement2, alt: "Placement 2" },
+  { id: 3, src: placement3, alt: "Placement 3" },
+  { id: 4, src: placement4, alt: "Placement 4" },
+  { id: 5, src: placement5, alt: "Placement 5" },
+  { id: 6, src: placement6, alt: "Placement 6" },
+  { id: 7, src: placement7, alt: "Placement 7" },
+  { id: 8, src: placement8, alt: "Placement 8" },
+  { id: 9, src: placement9, alt: "Placement 9" },
+  { id: 10, src: placement10, alt: "Placement 10" },
+  { id: 11, src: placement11, alt: "Placement 11" },
+  { id: 12, src: placement12, alt: "Placement 12" },
+  { id: 13, src: placement13, alt: "Placement 13" },
+  { id: 14, src: placement14, alt: "Placement 14" },
+  { id: 15, src: placement15, alt: "Placement 15" },
+  { id: 16, src: placement16, alt: "Placement 16" },
+];
+
+
+export const memorialEvents=[
+    {
+        name:"NAAC peer Team Visit",
+        img:naac_team
+    },
+    {
+        name:"NSCET Hackathon - 36 Hours Tech Marathon",
+        img:nscet_hack
+    },
+    {
+        name:"Skill India Award - 2nd Place",
+        img:tn_skills
+    },
+    {
+        name:"Kalam Young Achiever Awards - 24",
+        img:kalam_young
+    },
+    {
+        name:"WG Tech Solution Pvt.ltd Inauguration ceremony",
+        img:wg_tech
+    },
+    {
+        name:"State Level Science Expo",
+        img:science_expo
+    },  
+]
+
+export const culturals=[
+    {
+        name:"Annual Day 25",
+        img:annual_day
+    },
+    {
+        name:"78th Independence Day",
+        img:Independence,
+    },
+    {
+        name:"Thiru Onam Celebration",
+        img:onam
+    },
+    {
+        name:"Fresher’s Day",
+        img:freshers
+    },
+    {
+        name:"11th Graduation Day",
+        img:Graduation
+    },
+    {
+        name:"WAVES- 2024",
+        img:waves
+    },{
+        name:"Navaratri, Saraswathi & Ayudh Pooja Celebration",
+        img:saraswathi
+    },{
+        name:"76th Republic Day Celebration",
+        img:republic
+    },
+    {
+        name:"Womens Day Celebration",
+        img:womens_day
+    },
+]
+export const Sports=[
+    {
+        name: "OSCAR World Record – Silambam Multiweapons",
+      img: "silambam_record.jpg"
+      },
+      {
+        name: "Drug Awareness Cycle Rally 2025",
+      img: "cycle_rally.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Handball Women",
+      img: "handball_women.jpg"
+      },
+      {
+        name: "CFI Cycling National Championships",
+      img: "cycling_nationals.jpg"
+      },
+      {
+        name: "All India Inter University Boxing",
+      img: "boxing_interuniversity.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Power Lifting",
+      img: "powerlifting.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Cross Country Men",
+      img: "crosscountry_men.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Cross Country Women",
+      img: "crosscountry_women.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Weight Lifting Men",
+      img: "weightlifting_men.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Ball Badminton Women",
+      img: "ballbadminton_women.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Athletics",
+      img: "athletics_zone17.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Boxing Men",
+      img: "boxing_men.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Athletics",
+      img: "athletics_interzone.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Volleyball Women",
+      img: "volleyball_women.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Volleyball Men",
+      img: "volleyball_men.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Taekwondo Men",
+      img: "taekwondo_men.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Chess Men",
+      img: "chess_men.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Chess Women",
+      img: "chess_women.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Table Tennis Women",
+      img: "tabletennis_women.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Kabbadi Men",
+      img: "kabbadi_men.jpg"
+      },
+      {
+        name: "District Cycling Championship",
+      img: "district_cycling.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Basketball Women",
+      img: "basketball_women.jpg"
+      },
+      {
+        name: "CM Trophy Athletics (Men)",
+      img: "cmtrophy_athletics_men.jpg"
+      },
+      {
+        name: "CM Trophy Silambam (Men)",
+      img: "cmtrophy_silambam_men.jpg"
+      },
+      {
+        name: "CM Trophy Badminton Doubles (Men)",
+      img: "cmtrophy_badminton_doubles.jpg"
+      },
+      {
+        name: "CM Trophy Swimming (Men)",
+      img: "cmtrophy_swimming_men.jpg"
+      },
+      {
+        name: "CM Trophy Handball (Men)",
+      img: "cmtrophy_handball_men.jpg"
+      },
+      {
+        name: "CM Trophy Chess (Men)",
+      img: "cmtrophy_chess_men.jpg"
+      },
+      {
+        name: "CM Trophy Basketball (Women)",
+      img: "cmtrophy_basketball_women.jpg"
+      },
+      {
+        name: "State Cycling Championship",
+      img: "state_cycling.jpg"
+      },
+      {
+        name: "Anna University Zone-17 Basketball Men",
+      img: "basketball_men.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Handball Women – Selection Committee Member",
+      img: "handball_women_selection.jpg"
+      },
+      {
+        name: "Anna University Inter Zone Handball Women – Team Manager",
+      img: "handball_women_manager.jpg"
+      },
+      {
+        name: "Zone-17 Team Manager",
+      img: "zone17_teammanager.jpg"
+      },
+      {
+        name: "Zone-17 Volleyball Women – Selection Committee Member",
+      img: "volleyball_women_selection.jpg"
+      },
+      {
+        name: "Zone-17 Chess Men & Women – Selection Committee Member",
+      img: "chess_selection.jpg"
+      },
+      {
+        name: "Basketball Women – Selection Committee Member",
+      img: "basketball_women_selection.jpg"
+      },
+      {
+        name: "Badminton Women – Selection Committee Member",
+      img: "badminton_women_selection.jpg"
+      },
+      {
+        name: "Basketball Men – Selection Committee Member",
+      img: "basketball_men_selection.jpg"
+      }    
+]
